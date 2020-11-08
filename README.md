6eda8122d96e3eeae62626e0904518e5a4ffc06b
hgeorge <h.george16@yahoo.com>
2020-11-07 22:17:24 -0500

    Updating main and README

--- README.md
+++ README.md
@@ -25,3 +25,12 @@ cmake --CMAKE_TYPE=Release ..
 
 ## Project Struction
 TODO
+
+## TODO Item List
+1. Figure out how to define the boundary for input mesh... By user or using boundary loop?
+2. What PDE to support? (Prob. list them out and let user choose)
+	- $\delta u = 0, u = ||x|| on \del\omega$ (Laplace with Dirichlet BD)
+    - $\delta u = g, u = 0 on \del\omega$ (Poisson, need to think about $g$)
+    - Maybe biharmonic? (can show the parameterization using this biharmonic tehnique LOL)
+    - let's have a couple gradient and hessian in there as well :)
+3. Add the data folder, find some good meshes
--- main.cpp
+++ main.cpp
@@ -1,27 +1,51 @@
 #include <Eigen/Core>
 #include <igl/read_triangle_mesh.h>
 #include <igl/opengl/glfw/Viewer.h>
+#include <igl/AABB.h>
+#include <igl/parula.h>
+#include <igl/colormap.h>
 
 #include <iostream>
 
 int main(int argc, char* argv[]) {
-	
+	// load mesh
 	Eigen::MatrixXd V;
 	Eigen::MatrixXi F;
 	igl::read_triangle_mesh((argc > 1 ? argv[1] : "../data/smthing.obj"), V, F);
 
+	// set up AABB tree
+	igl::AABB<Eigen::MatrixXd, 3> AABB_tree;
+	AABB_tree.init(V, F);
+
 	igl::opengl::glfw::Viewer viewer;
 	std::cout << R"()";
 
-	const auto update = [&]() {
+	double scale = 100.;
+	bool show_org_mesh = true;
 
+	const auto update = [&]() {
+		if (show_org_mesh) {
+			Eigen::MatrixXd C;
+			igl::parula(V, -scale, scale, C);
+			viewer.data().set_colors(C);
+		}
+		else {
+			Eigen::MatrixXd CM;
+			igl::colormap(igl::COLOR_MAP_TYPE_VIRIDIS, V, -scale, scale, CM);
+			viewer.data().set_colormap(CM);
+		}
 	};
 
 	viewer.callback_key_pressed = [&](igl::opengl::glfw::Viewer&, unsigned int key, int mod)
 	{
 		switch (key)
 		{
-
+		case 'M':
+		case 'm':
+			show_org_mesh ^= 1;
+			break;
+		default:
+			return false;
 		}
 		update();
 		return true;
