22663333a8943af9c93daf7eba040640305f5fc2
George <h.george16@yahoo.com>
2020-12-06 11:46:35 -0500

    tried something and fixed sphere random gen

--- main.cpp
+++ main.cpp
@@ -14,14 +14,16 @@ int main(int argc, char* argv[]) {
 	igl::read_triangle_mesh((argc > 1 ? argv[1] : "../data/bunny.off"), V, F);
 
 	igl::opengl::glfw::Viewer viewer;
-	viewer.data().point_size = 5;
 	const int xid = viewer.selected_data_index;
+    viewer.data().point_size = 5;
 	viewer.append_mesh();
+	const int yid = viewer.selected_data_index;
+    viewer.data().point_size = 8;
 
 	std::cout << R"(
     [space] Solves the PDE with current sampled points
-    P       Show the sampled points
-    p       Hide the sampled points
+    H,h     Hide boundary points
+    S,s     Show boundary points
     +       Double the number of sample points
     -       Halve the number of sample points
     R,r     resets the sampled points
@@ -31,43 +33,47 @@ int main(int argc, char* argv[]) {
 
     // sample the points
     int n_samples = 1048576;
-    bool show_samples = true;
+    bool show_boundary = true;
+    double solve_time;
     Eigen::MatrixXd P;
     Eigen::VectorXd U;
     Eigen::VectorXd B;
+    Eigen::MatrixXd BCM;
 
 	// TODO: add a few more boundary conditions
 	int g_idx = 0;
-	std::vector<const std::function<double(const Eigen::Vector3d&)>> boundary_funcs;
-    boundary_funcs.emplace_back([](const Eigen::Vector3d &x) -> double { return x.maxCoeff(); });
+	std::vector<std::function<double(const Eigen::Vector3d&)>> boundary_funcs;
+    boundary_funcs.emplace_back([](const Eigen::Vector3d &x) -> double { return x(0); });
     boundary_funcs.emplace_back([](const Eigen::Vector3d &x) -> double { return 2 * x.norm(); });
+    boundary_funcs.emplace_back([](const Eigen::Vector3d &x) -> double { return 5.; });
 
-    std::vector<const std::string> boundary_strings;
-    boundary_strings.emplace_back("||x||_1");
-    boundary_strings.emplace_back("2 * ||x||_2");
+    std::vector<std::string> boundary_strings;
+    boundary_strings.emplace_back("g(x) = x_0");
+    boundary_strings.emplace_back("g(x) = 2 * ||x||");
+    boundary_strings.emplace_back("g(x) = 5");
 
-    const auto &set_boundary = [&](const std::function<double(const Eigen::Vector3d&)> g) {
+    const auto &set_boundary = [&](std::function<double(const Eigen::Vector3d&)> g) {
         B.resize(V.rows());
         for (int i = 0; i < V.rows(); i++) {
             B(i) = g(V.row(i));
         }
+        igl::colormap(igl::COLOR_MAP_TYPE_MAGMA, V, B.minCoeff(), B.maxCoeff(), BCM);
+        viewer.data_list[yid].set_points(V, BCM);
     };
-
-	const auto update = [&]() {
-	    Eigen::MatrixXd CM;
-	    igl::colormap(igl::COLOR_MAP_TYPE_VIRIDIS, V, -100, 100, CM);
-	    viewer.data().set_colormap(CM);
-	};
 
 	const auto& set_points = [&]()
 	{
 		random_sampling(V, F, n_samples, P);
         const Eigen::RowVector3d orange(1.0, 0.7, 0.2);
-		if (show_samples)
-			viewer.data_list[xid].set_points(P, (1. - (1. - orange.array()) * .8));
-		else
-			viewer.data_list[xid].clear_points();
+        viewer.data_list[xid].set_points(P, (1. - (1. - orange.array()) * .8));
 	};
+	const auto& show_bndry_pts = [&]()
+    {
+        if (show_boundary)
+            viewer.data_list[yid].set_points(V, BCM);
+        else
+            viewer.data_list[yid].clear_points();
+    };
 
 	const auto& solve = [&]()
 	{
@@ -77,13 +83,18 @@ int main(int argc, char* argv[]) {
 		viewer.data_list[xid].set_points(P, CM);
 	};
 
+    const auto update = [&]() {
+        show_bndry_pts();
+    };
+
 	viewer.callback_key_pressed = [&](igl::opengl::glfw::Viewer&, unsigned int key, int mod)
 	{
 		switch (key)
 		{
 		case ' ':
 		    std::cout << "Solving...\n";
-			std::cout << "Solve time: " << timer(solve) << " sec\n";
+		    solve_time = timer(solve);
+			std::cout << "Solve time: " << solve_time << " sec\n";
 			break;
 		case '+':
 			n_samples *= 2;
@@ -95,11 +106,13 @@ int main(int argc, char* argv[]) {
 			set_points();
 			std::cout << "# Sample points: " << P.rows() << "\n";
 			break;
-		case 'P':
-			show_samples = true;
+        case 'S':
+		case 's':
+			show_boundary = true;
 			break;
-		case 'p':
-			show_samples = false;
+		case 'H':
+		case 'h':
+			show_boundary = false;
 			break;
         case 'G':
         case 'g':
@@ -121,7 +134,8 @@ int main(int argc, char* argv[]) {
 	viewer.data().set_mesh(V, F);
     set_boundary(boundary_funcs[g_idx]);
     std::cout << "Boundary condition: " << boundary_strings[g_idx] << "\n";
-	std::cout << "Time used: " << timer(set_points) << " sec\n";
+	set_points();
+    std::cout << "# Sample points: " << P.rows() << "\n";
 
 	update();
 	viewer.data().show_lines = false;
--- src/walk_on_spheres.cpp
+++ src/walk_on_spheres.cpp
@@ -4,6 +4,8 @@
 
 #include <igl/AABB.h>
 #include <igl/slice_mask.h>
+#include <igl/slice_into.h>
+#include <numeric>
 
 void walk_on_spheres(const Eigen::MatrixXd& V, const Eigen::MatrixXi& F, const Eigen::VectorXd& B,
 	const Eigen::MatrixXd& P, Eigen::VectorXd& U) 
@@ -11,34 +13,43 @@ void walk_on_spheres(const Eigen::MatrixXd& V, const Eigen::MatrixXi& F, const E
 	igl::AABB<Eigen::MatrixXd, 3> aabb;
 	aabb.init(V, F);
 
-	const float tol = 1e-4;
+	const float tol = 1e-3;
 	const int max_itr = 5;
 	const int n_walks = 64;
 
-	U = Eigen::VectorXd::Zero(P.rows());
+    Eigen::MatrixXd C;
+    Eigen::VectorXd D;
+    Eigen::VectorXi I;
+    U = Eigen::VectorXd::Zero(P.rows());
+
+    const auto& handle_done_pts = [&](const Eigen::VectorXi &idx_, const Eigen::VectorXi &I_) {
+        Eigen::Vector3d phi;
+        for (int j = 0; j < I_.rows(); j++) {
+            // interpolate between the three vertices and find value
+            int f = I_(j);
+            interpolate(C.row(idx_(j)), V.row(F(f, 0)), V.row(F(f, 1)), V.row(F(f, 2)), phi);
+            U(idx_(j)) += phi(0) * B(F(f,0)) + phi(1) * B(F(f,1)) + phi(2) * B(F(f,2));
+        }
+    };
+
 	// start the random walk
 	for (int i = 0; i < n_walks; i++) {
 		Eigen::MatrixXd X = P;
 		Eigen::VectorXd R = std::numeric_limits<double>::max() * Eigen::VectorXd::Ones(P.rows());
+        Eigen::VectorXi idx = Eigen::ArrayXi::LinSpaced(P.rows(), 0, P.rows()-1);
 
-		int itr = 0;
-		Eigen::VectorXd sqrD;
-		Eigen::VectorXi I;
-		Eigen::MatrixXd C;
+        int itr = 0;
 		while (itr < max_itr) {
 			// find closest point on the boundary and change radius
-			aabb.squared_distance(V, F, X, sqrD, I, C);
-			R = R.cwiseMin(sqrD.cwiseSqrt());
+			aabb.squared_distance(V, F, X, D, I, C);
+            D = D.cwiseSqrt();
+			R = R.cwiseMin(D);
 			pts_on_spheres(R, X);
+
 			itr++;
 		}
-
-		for (int j = 0; j < I.rows(); j++) {
-			// interpolate between the three vertices and find value
-			Eigen::Vector3d phi;
-			interpolate(C.row(j), V.row(F(I(j), 0)), V.row(F(I(j), 1)), V.row(F(I(j), 2)), phi);
-			U(j) += phi(0) * B(F(I(j),0)) + phi(1) * B(F(I(j),1)) + phi(2) * B(F(I(j),2));
-		}
+		// handle the rest of the points
+		handle_done_pts(idx, I);
 	}
 	U = U / n_walks;
 }
